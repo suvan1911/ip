d9a1a2f6339de17c0345965c4ea08e88f6fd72f7
Suvan <suvanhanda01@gmail.com>
2026-08-21 15:46:38 +0800

    Add greet and exit

--- src/main/java/LenZaBot.java
+++ src/main/java/LenZaBot.java
@@ -9,5 +9,16 @@ public class LenZaBot {
         """;
 
         System.out.println(banner);
+        greet();
+        quit();
+    }
+
+    public static void greet() {
+        System.out.println("Hi. This is Lenza. What do you want to do?");
+    }
+
+    public static void quit() {
+        System.out.println("Bye! See ya later.");
+        System.exit(0);
     }
 }

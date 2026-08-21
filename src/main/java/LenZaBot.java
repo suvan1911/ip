2b7e1b92949b65ccc1adb3ee93b3e962a93bfb4e
Suvan <suvanhanda01@gmail.com>
2026-08-21 16:22:40 +0800

    Add tasks and list

--- src/main/java/LenZaBot.java
+++ src/main/java/LenZaBot.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class LenZaBot {
     private static final String BYE_COMMAND = "bye";
+    private static final String LIST_COMMAND = "list";
+
+    private static int numTasks = 0;
+    private static String[] tasks = new String[100];
     public static void main(String[] args) {
         String banner = """
 ██      ███████ ███    ██ ███████  █████  ██████   ██████  ████████ 
@@ -19,13 +23,12 @@ public class LenZaBot {
             System.out.print(">  ");
             String command = scanner.nextLine();
 
-            if (command.equals(BYE_COMMAND)) {
-                quit();
+            switch (command) {
+                case BYE_COMMAND -> quit();
+                case LIST_COMMAND -> printTasks();
+                default -> addTask(command);
             }
-
-            System.out.println(command);
         }
-
     }
 
     public static void greet() {
@@ -35,5 +38,19 @@ public class LenZaBot {
     public static void quit() {
         System.out.println("Bye! See ya later.");
         System.exit(0);
+    }
+
+    public static void addTask(String task) {
+        tasks[numTasks] = task;
+        numTasks++;
+        System.out.println(String.format("Added task: %s", task));
+    }
+
+    public static void printTasks() {
+        for (int i = 0; i < numTasks; i++) {
+            System.out.println(
+                String.format("%d. %s", i + 1, tasks[i])
+            );
+        }
     }
 }
